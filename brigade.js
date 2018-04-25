740108d952679e32b3203615b80db216d72b78de
Patrick Rogers <patrick@gfycat.com>
2018-04-25 10:29:44 -0700

    Pass $BUILD to training job.

--- brigade.js
+++ brigade.js
@@ -1,4 +1,6 @@
-const { events, Job } = require("brigadier");
+// An brigade pipeline for Gfycat
+
+const { events, Job } = require("brigadier"); // eslint-disable-line node/no-missing-require
 
 events.on("train", async (event, project) => {
   const payload = JSON.parse(event.payload);
@@ -11,12 +13,13 @@ events.on("train", async (event, project) => {
     ACCOUNT_KEY: project.secrets.azure_key,
     CLASSIFIER: classifier,
     TRAINING_STEPS: trainingSteps,
+    BUILD: event.buildID,
   };
 
   train.env = env;
   train.imagePullSecrets = "acrcredentials";
   train.imageForcePull = true;
-  train.timeout = 120 * 60 * 1000;
+  train.timeout = 90 * 60 * 1000;
 
   const result = await train.run();
   const name = result.data.trim();
